d65e3800158aa28c4d0bd1da87718c700aee7736
00CLPLC00 <97087591+00CLPLC00@users.noreply.github.com>
2023-04-14 10:35:54 +0200

    Update sketch.js
    Modifications du code pour l'Ajout d'images  et d'un commentaire dans la présentation des progressions

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
+let img;
+let img2;
+let imgindic;
+
 // La liste de progressions harmoniques
-let progressions = [
-  ['vi', 'IV', 'I', 'V'],
+let progressions = [  ['vi', 'IV', 'I', 'V'],
   ['I', 'V', 'vi', 'IV'],
   ['iii', 'vi', 'IV', 'I'],
   ['I', 'IV', 'V'],
@@ -24,9 +27,14 @@ let progressions = [
 
 let currentProgression = null;
 
+function preload() {
+  img = loadImage("https://i.imgur.com/2wzqqNb.png");
+  img2 = loadImage("https://i.imgur.com/OEKIlJc.png");
+  imgindic = loadImage("https://i.imgur.com/aBqjZQn.png");
+}
+
 function setup() {
-  createCanvas(475, 150);
-  background(44, 62, 80 );
+  createCanvas(500, 500);
   
   // Créer un bouton pour sélectionner une progression harmonique
   let button = createButton("C'est ici pour s'entrainer sur des progression harmonique célèbre POP/ROCK");
@@ -34,6 +42,14 @@ function setup() {
 }
 
 function draw() {
+  background(93, 109, 126 );
+  // Afficher l'image en haut à gauche du canvas
+  imageMode(CORNER);
+  image(img, 0, 0, 100, 100);
+  // Afficher l'image en haut à droite du canvas
+  imageMode(CORNER);
+  image(img2, width - 100, 0, 100, 100);
+  
   // Vérifier que la variable currentProgression n'est pas null avant de l'afficher
   if (currentProgression != null) {
     // Afficher la progression harmonique courante
@@ -41,8 +57,14 @@ function draw() {
     textSize(50);
     fill(240, 243, 244);
     text(currentProgression.join(" - "), width/2, height/2);
-    
   }
+  else {
+    textSize(30);
+    text("HEY HO ! LET'S GO !", width/4, height/2);
+  }
+  // Afficher l'image en bas centrer du canvas
+  imageMode(CENTER);
+  image(imgindic, width - 270, 405, 450,175);
 }
 
 function selectRandomProgression() {
@@ -51,5 +73,5 @@ function selectRandomProgression() {
   currentProgression = progressions[randomIndex];
   
   // Redessiner l'arrière-plan
-  background(44, 62, 80 );
+  background(93, 109, 126 );
 }
